3d02eb0d23f540077dd2468dfd9e17c74e13408e
15236097327 <162610759+15236097327@users.noreply.github.com>
2024-05-12 16:49:22 +0800

    Add files via upload

---
+++ 第四次作业第一题.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+bool cmp(int a, int b)
+{
+	return a > b;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int>p;
+	while (n--)
+	{
+		int x;
+		cin >> x;
+		p.push_back(x);
+	}
+	sort(p.begin(), p.end());
+	vector<int>num;
+	vector<int>ans;
+	int flag = 1;
+	int i;
+	for ( i = 0;i < p.size() - 1;i++)
+	{
+		if (p[i] == p[i + 1] - 1)
+		{
+			flag++;
+		}
+		else
+		{
+			num.push_back(flag);
+			flag = 1;
+		}
+	}
+	if (i ==p.size()-1)
+	{
+		num.push_back(flag);
+	}
+	sort(num.begin(), num.end(),cmp);
+	cout << num[0] << endl;
+	flag = 1;
+	for (int i = 0;i < p.size()-1;i++)
+	{
+		if (p[i] == p[i + 1] - 1)
+		{
+			flag++;
+			ans.push_back(p[i]);
+		}
+		if (p[i] != p[i + 1] - 1)
+		{
+			ans.clear();
+		}
+		if (flag == num[0]&&flag>1)
+		{
+			int j;
+			ans.push_back(ans[ans.size() - 1] + 1);
+			for ( j = 0;j <= flag - 1;j++)
+			{
+				cout << ans[j] << " ";
+			}
+			cout << endl;
+			flag = 1;
+		}
+	}
+
+
+
+
+	
+}
