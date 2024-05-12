859fae91dbea532e8a58a0fab9a16dce71b4d767
15236097327 <162610759+15236097327@users.noreply.github.com>
2024-05-12 16:51:46 +0800

    Add files via upload

---
+++ 第四次作业第五题.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	int* dp = new int[s.length()];
+	for(int i=0;i<s.length();i++)
+	{
+		dp[i] = 1;
+	}
+	int maxvalue = 0;
+	for (int i = 0;i < s.length();i++)
+	{
+		for (int j = 0;j < i;j++)
+		{
+			if (s[i] >= s[j])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		maxvalue = max(maxvalue, dp[i]);
+	}
+	cout << maxvalue;
+
+
+}
